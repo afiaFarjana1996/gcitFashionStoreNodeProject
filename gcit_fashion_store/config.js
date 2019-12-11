e1794c7c0f04fbf918b2932837ca33441046abb8
afiaFarjana1996 <afia.farjana@smoothstack.com>
2019-12-11 12:27:32 -0500

    changed docker file

--- gcit_fashion_store/config.js
+++ gcit_fashion_store/config.js
@@ -5,12 +5,6 @@ let CONFIG = {} //Make this global to use all over the application
 CONFIG.app          = process.env.APP
 CONFIG.port         = process.env.PORT || 3000
 
-// if(process.env.NODE_ENV == 'TEST'){
-//   CONFIG.db_host      = '127.0.0.1'
-//   CONFIG.db_user      = 'root'
-//   CONFIG.db_password  = 'smoothstack'
-// }
-// else{ }
   CONFIG.db_host      = process.env.DB_HOST
   CONFIG.db_user      = process.env.DB_USER
   CONFIG.db_password  = process.env.DB_PASSWORD
